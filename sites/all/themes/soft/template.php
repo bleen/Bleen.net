ac96a71f07c6d02ae2d782ee1415e52f2e77b71c
bleen <git.119x@bleen.net>
2011-12-11 17:49:24 -0500

    adding a new page tpl and removing some default core css files
    mend

--- sites/all/themes/soft/template.php
+++ sites/all/themes/soft/template.php
@@ -1,12 +1,14 @@
 <?php
-
 /**
- * @file
- * This file is empty by default because the base theme chain (Alpha & Omega) provides
- * all the basic functionality. However, in case you wish to customize the output that Drupal
- * generates through Alpha & Omega this file is a good place to do so.
- * 
- * Alpha comes with a neat solution for keeping this file as clean as possible while the code
- * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
- * for more information on this topic.
+ * Your themes template.php file
  */
+function soft_css_alter(&$css) {
+  // Turn off some styles from the system module.
+  unset($css[drupal_get_path('module', 'system') . '/system.messages.css']);
+  unset($css[drupal_get_path('module', 'system') . '/system.menus.css']);
+}
+
+function soft_preprocess_page(&$variables) {
+  kpr($variables);
+  dargs();
+}
